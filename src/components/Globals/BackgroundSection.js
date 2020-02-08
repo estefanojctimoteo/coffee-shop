9954b82ca2f519b8b6ea91518b84ec819a38adb3
Estefano Timoteo <estefano.juliano@gmail.com>
2020-02-08 12:58:27 -0300

    creating background

---
+++ src/components/Globals/BackgroundSection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import BackgroundImage from 'gatsby-background-image'
+export default function BackgroundSection({img, styleClass, title, children}) {
+  return (
+    <div>
+      hello from background
+    </div>
+  )
+}
