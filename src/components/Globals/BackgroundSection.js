e406920280eb211410b441ea3e6e4265a852c7fa
Estefano Timoteo <estefano.juliano@gmail.com>
2020-02-08 22:27:37 -0300

    Terminando BackgroundImage

--- src/components/Globals/BackgroundSection.js
+++ src/components/Globals/BackgroundSection.js
@@ -2,8 +2,11 @@ import React from 'react'
 import BackgroundImage from 'gatsby-background-image'
 export default function BackgroundSection({img, styleClass, title, children}) {
   return (
-    <div>
-      hello from background
-    </div>
+    <BackgroundImage className={styleClass} fluid={img}>
+      <h1 className='title text-white text-uppercase text-center display-4 font-weight-bold'>
+        {title}
+      </h1>
+      {children}
+    </BackgroundImage>
   )
 }
