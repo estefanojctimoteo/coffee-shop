021cdb3ae5215d1949d2951212a53128b5c3dea6
Estefano Juliano Cecilio Timoteo - AeC <estefano.timoteo@grupoaec.com.br>
2020-03-02 08:29:50 -0300

    start rendering items

--- src/components/Home/Menu.js
+++ src/components/Home/Menu.js
@@ -6,13 +6,39 @@ import Img from 'gatsby-image'
 export default class Menu extends Component {
   constructor(props) {
     super(props)
-    console.log(props.items)
+    this.state = {
+      items: props.items.edges,
+      coffeeItems: props.items.edges,
+    }
   }
   render() {
-    return (
-      <div>
-        content from menu component
-      </div>
-    )
+    if (this.state.items.length > 0) {
+      return (
+        <section className="menu py-5">
+          <div className="container">
+            <Title title="best of our menu" />
+            {/* categories */}
+            {/* items */}
+            <div className="row">
+              {this.state.coffeeItems.map({node})}
+            </div>
+          </div>
+        </section>
+      )
+    }
+    else {
+      return (
+        <section className='menu py-5'>
+          <div className='container' >
+            <Title title='best of our menu' />
+            <div className='row' >
+              <div className='col-10 col-sm-6 mx-auto text-center text-capitalize' >
+                <h1>there are no items to display</h1>
+              </div>
+            </div>
+          </div>
+        </section>
+      )
+    }
   }
 }
